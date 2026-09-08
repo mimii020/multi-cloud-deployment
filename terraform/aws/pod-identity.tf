cef8c70f1b54c3b03e0b067053269de7a9403b1a
mimii020 <imen.abidi@insat.ucar.tn>
2026-09-09 01:44:00 +0200

    fixed aws vars

--- terraform/aws/pod-identity.tf
+++ terraform/aws/pod-identity.tf
@@ -1,6 +1,5 @@
 resource "aws_eks_pod_identity_association" "backend" {
-  cluster_name = module.eks.cluster_name
-  namespace = "backend"
+  cluster_name = aws_eks_cluster.main.name  namespace = "backend"
   service_account = "backend-sa"
   role_arn = aws_iam_role.backend_pod.arn
 }
