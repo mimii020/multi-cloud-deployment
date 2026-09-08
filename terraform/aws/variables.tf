daef5744ef6b64c6725a053be269ada04e30510d
mimii020 <imen.abidi@insat.ucar.tn>
2026-09-09 01:40:52 +0200

    fixed aws vars

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,21 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "project_name" {
-  description = "Project name"
-  type        = string
-  default     = "multicloud-devops"
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "dev"
-}
-
 variable "subnet_ids" {
   description = "List of subnet IDs for EKS"
   type        = list(string)
