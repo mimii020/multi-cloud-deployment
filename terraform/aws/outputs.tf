f148c3c655981910ec99e51298b533622adbb9b4
mimii020 <imen.abidi@insat.ucar.tn>
2026-09-09 01:42:37 +0200

    fixed aws vars

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,24 +1,19 @@
 output "cluster_name" {
   description = "EKS cluster name"
-  value = module.eks.cluster_name
+  value       = aws_eks_cluster.main.name
 }
 
 output "cluster_endpoint" {
   description = "EKS cluster endpoint"
-  value = module.eks.cluster_endpoint
+  value       = aws_eks_cluster.main.endpoint
 }
 
 output "region" {
   description = "AWS region"
-  value = var.aws_region
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value = module.vpc.vpc_id
+  value       = var.aws_region
 }
 
 output "backend_s3_bucket" {
   description = "Backend S3 bucket"
-  value = aws_s3_bucket.backend.bucket
+  value       = aws_s3_bucket.backend.bucket
 }
